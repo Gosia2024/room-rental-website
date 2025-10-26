ff834461d2dd7f27c28be6f8323354fed413c032
Gosia2024 <lm.sprachen@gmail.com>
2025-10-26 21:23:30 +0100

    feat: add components and fix imports

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import Header from '../Header/Header'
+import Hotels from '../Hotels/Hotels'
+import Menu from '../Menu/Menu'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     
+     <Header />
+     <Menu />
+     <Hotels/>
+    </>
+  )
+}
+
+export default App
