7e9c7da1216eae22f08512b3fe37fd7d8bc438c5
Gosia2024 <lm.sprachen@gmail.com>
2025-10-27 10:53:03 +0100

    feat: install Bootstrpa via npm and import CSS in main.jsx

--- src/components/Header/Searchbar/Searchbar.jsx
+++ src/components/Header/Searchbar/Searchbar.jsx
@@ -1,15 +1,15 @@
 export default function Searchbar(){
-    const styles = { borderRadius: 8}
+    const styles = { borderRadius: 8, marginRight: 5}
     return (
 
-        <>
+        <div className="d-flex">
          <input 
          placeholder='Szukaj...' 
         style={styles}
-        className =""
+        className ="form-control"
         />
         <button className="btn btn-primary">Szukaj</button>
-       </>
+       </div>
 
     )
 }
